c7d0b0344d4d00ae1770027719e193609d23d560
Human Computer Integration Lab <hci@uchicago.edu>
2021-11-16 16:41:32 -0600

    Update OxygenBar.cs

--- Assets/Scripts/OxygenBar.cs
+++ Assets/Scripts/OxygenBar.cs
@@ -9,11 +9,13 @@ public class OxygenBar : MonoBehaviour
 
     private Slider slider;
     public InformationManager iM;
-
+    private Text textSliderValue;
     // Start is called before the first frame update
     void Start()
     {
         slider = GetComponent<Slider>();
+	textSliderValue = GetComponent<Text>();
+	ShowSliderValue();
         SetMaxOxygenLevel(iM.GetMaxOxygenLevel());
     }
 
@@ -21,11 +23,21 @@ public class OxygenBar : MonoBehaviour
     {
         slider.maxValue = maxLevel;
         slider.value = maxLevel; // Start with max!
+	Debug.Log(slider.value);
+	Debug.Log("gkasdgasjhgd");
     }
 
     public void SetOxygenLevel (float level)
     {
         slider.value = level;
+	Debug.Log(level);
+	if(level == 15){
+		Debug.Log("UR DROWNING");
+	}
+    }
+    public void ShowSliderValue () {
+    	string sliderMessage = "OXYGEN LEVEL= " + slider.value;
+    	textSliderValue.text = sliderMessage;
     }
 
     private void Update()
